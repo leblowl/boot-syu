940f42cd4e6a805c32e0e74f8a4e2e067d72f684
Lucas Leblow <leblowl@gmail.com>
2015-05-14 12:14:32 -0700

    working well!

--- build.boot
+++ build.boot
@@ -3,7 +3,8 @@
   :resource-paths #{"resources"}
   :source-paths #{"src"}
   :dependencies '[[org.clojure/clojure "1.6.0"]
-                  [version-clj "0.1.2"]])
+                  [slingshot "0.12.2"]
+                  [ancient-clj "0.3.9"]])
 
 (require
   '[boot-update-deps.app :as app])
--- src/boot_update_deps/app.clj
+++ src/boot_update_deps/app.clj
@@ -1,29 +1,37 @@
 (ns boot-update-deps.app
   (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]
-            [boot.core :as boot :refer [deftask]])
-  (:import [java.io PushbackReader]))
+            [clojure.string :as str]
+            [boot.core :as boot :refer [deftask set-env!]]
+            [ancient-clj.core :as ancient]))
 
-(def all-jars-url "http://clojars.org/repo/all-jars.clj")
-(def all-jars
-  (with-open [r (PushbackReader. (io/reader all-jars-url))]
-    (let [edn-seq (repeatedly (partial edn/read {:eof :EOF} r))]
-      (doall (take-while #(not= :EOF %) edn-seq)))))
+(defn get-latest-deps [deps opts]
+  (let [get-latest-version #(ancient/artifact-outdated-string? % opts)
+        artifacts (map ancient/read-artifact deps)
+        latest-versions (remove nil? (map get-latest-version artifacts))]
+    (map vector (map first deps) latest-versions)))
 
-(defn in?
-  "true if seq contains elm"
-  [seq elm]
-  (some #(= elm %) seq))
+(defn replace-deps [deps latest-deps]
+  (let [f "build.boot"
+        dep-strs (map str deps)
+        latest-dep-strs (map str latest-deps)
+        replacements (map vector dep-strs latest-dep-strs)]
+    (spit f (reduce #(apply str/replace %1 %2) (slurp f) replacements))))
 
-(defn update [deps]
-  (let [dep-names (map (comp str first) deps)]
-    (filter #(in? dep-names (-> % first str)) all-jars)))
-
-(defn replace [deps])
+(defn syu
+  ([]
+   (syu {:snapshots? false :qualified? false}))
+  ([opts]
+   (let [deps (:dependencies (boot/get-env))
+         latest-deps (get-latest-deps deps opts)]
+     (replace-deps deps latest-deps))))
 
 (deftask update-deps
-  "Update dependencies"
-  []
+  "Update all project dependencies."
+  [s snapshots bool "include SNAPSHOT versions"
+   q qualified bool "include alpha, beta, etc... versions"
+   a all       bool "include snapshots and qualified versions"]
   (boot/with-pre-wrap fileset
-    (update (:dependencies (boot/get-env)))
+    (let [opts {:snapshots? (or snapshots all)
+                :qualified? (or qualified all)}]
+      (syu opts))
     fileset))
